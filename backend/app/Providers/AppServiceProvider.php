78b6a925f595030cae4bff989b82555c25c79af7
Michael Kilunga <mickworks30@gmail.com>
2026-03-24 11:40:41 +0300

    corrected email link

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         \App\Models\Task::observe(\App\Observers\TaskObserver::class);
         
         \Illuminate\Auth\Notifications\VerifyEmail::createUrlUsing(function ($notifiable) {
-            $frontendUrl = env('FRONTEND_URL', 'http://localhost:5173');
+            $frontendUrl = config('app.frontend_url');
             return "{$frontendUrl}/verify-email/{$notifiable->getKey()}/" . sha1($notifiable->getEmailForVerification());
         });
 
